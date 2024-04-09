f6c554969356baddfc8594a1db9dd639e8e65f66
GOΠZO <37483725+GONZOsint@users.noreply.github.com>
2024-04-09 17:13:53 +0200

    Update setup.py

--- setup.py
+++ setup.py
@@ -5,10 +5,11 @@ setup(
     author='GONZO',
     version="0.0.1",
     packages=find_packages(),
-    url='https://github.com/GONZOsint/factcheckexplorer'
     install_requires=[
         'requests',
     ],
     description='A Python library for fetching fact-checking data from Google Fact Check Explorer.',
+    long_description=open('README.md').read(),
+    long_description_content_type='text/markdown',
     url='https://github.com/GONZOsint/factcheckexplorer',
 )
